db931f522580047d591c6aca11bb1b040125db5b
Finn <finnfinite@yahoo.com>
2015-10-06 14:35:28 -0600

    Timer implementation, main menu scene added

--- Mess Motors Alpha/Assets/Scripts/Controller.cs
+++ Mess Motors Alpha/Assets/Scripts/Controller.cs
@@ -1,16 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Controller : MonoBehaviour {
 
 	public int frameWidth = 1920;
 	public int frameHeight = 1080;
-	private int boxSize = 60;
+    private int boxSize = 60;
+    public float timer;
+    public GameObject timerObj;
+
 
 	public Transform paintBox;
 
 	void Awake () {
-		int rows = frameHeight / boxSize;
+        int rows = frameHeight / boxSize;
 		int cols = frameWidth / boxSize;
 		for (int y = 0; y < rows; y++) {
 			for (int x = 0; x < cols; x++) {
@@ -27,6 +31,16 @@ public class Controller : MonoBehaviour {
         if (Input.GetKey("r"))
         { //resets the game if the r key is hit
             Application.LoadLevel(Application.loadedLevel);
+
+      }
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
         }
+        else { timer = 0; }
+        
+        timerObj.GetComponent<Text>().text = timer.ToString();
+
     }
 }
+
---
+++ Mess Motors Alpha/Assets/Scripts/MenuUI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuUI : MonoBehaviour {
+
+    public GameObject Start;
+    public GameObject Levels;
+    public GameObject Players;
+
+    public void ShowLevelSelect()
+    {
+        //start off, players off, levels on
+    }
+
+    public void StartGame()
+    {
+        Application.LoadLevel(0);
+        Debug.Log("westarted");
+        //start on, players off, levels off
+    }
+
+    public void LoadLevel (int level)
+    {
+        Application.LoadLevel(level);
+    }
+
+    public void Play()
+    {
+        //start off, players on, levels off
+    }
+
+}
